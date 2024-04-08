0babaf9d4f2f1e001bd2d40e0ced887910946d45
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-04-08 13:30:15 +0200

    try single page

--- server.js
+++ server.js
@@ -13,12 +13,11 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// Get tmdb data
+// Get tmdb url
 const base_url = "https://api.themoviedb.org/3";
 
 // API fetchen met Promise
-async function getData(page) {
+async function getPeople(page) {
     const api_url = base_url + "/person/popular?&page=" + page + "&" + process.env.API_Key;
 
     return fetch(api_url)
@@ -32,7 +31,7 @@ async function getData(page) {
 // Zie prompts: https://chemical-bunny-323.notion.site/API-Chat-GPT-Doc-372f65d6b2a5497a86b02ed94edffe17#ecf993846c754b9cae95d048caf153b8
 app.get('/', async function(req, res) {
     try {
-        const data = await getData(1);
+        const data = await getPeople(1);
 
         res.render('pages/index', {
             page: 1,
@@ -47,23 +46,31 @@ app.get('/', async function(req, res) {
 
 // Get page after button clicked
 app.post('/choice', async function(req, res) {
-    console.log(parseInt(req.body.page) + 1)
-
     res.redirect(`/${parseInt(req.body.page) + 1}`)
-});
-
-// Single page
-app.get('/single', function(req, res) {
-    res.render('pages/single');
 });
 
 // Get the page 
 app.get('/:page', async function(req, res) {
     try {
-        const data = await getData(req.params.page);
+        const data = await getPeople(req.params.page);
 
         res.render('pages/index', {
             page: req.params.page,
+            data
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching data');
+    }
+});
+
+// Single page
+app.get('/:id', async function(req, res) {
+    try {
+        const data = await getPeople(req.params.id);
+
+        res.render('pages/single', {
             data
         });
 
